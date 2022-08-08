43a07709ee4a3acde89c468bdcd5678647c2062d
Sameer <80711125+Sameony@users.noreply.github.com>
2022-08-08 20:01:52 +0530

    update database.js for private endpoint management

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize("resultManagement","docker","SamSakIB2001&@&",{
     dialect: "mysql",
-    host: "azuredatabasefor1.mysql.database.azure.com"
+    host: "privatedb.mysql.database.azure.com"
 });
 
 module.exports = sequelize;
