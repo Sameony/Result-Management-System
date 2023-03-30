43fc1de11a96ce03755d5b426005d996d74214ac
Sameer <80711125+Sameony@users.noreply.github.com>
2023-03-30 22:33:59 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize("resultManagement","docker","SamSakIB2001&@&",{
     dialect: "mysql",
-    host: "privatedb.mysql.database.azure.com"
+    host: "mysqlserverunique.mysql.database.azure.com"
 });
 
 module.exports = sequelize;
